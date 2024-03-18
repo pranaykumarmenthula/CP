8748ba507ed454859aa585207814a327e051a9c6
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2024-03-18 13:49:31 +0530

    Create DailyTemperatures.py

---
+++ DailyTemperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans=[0]*len(temperatures)
+        stack=[]
+        for i,temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+                index=stack.pop()
+                ans[index]=i-index
+            stack.append(i)
+        return ans 
